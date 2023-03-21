6ef526008fcc595b0cf53520ff9d72bb5883e8c5
Geronimo-Yowzah <panupongpnp02@gmail.com>
2023-03-21 23:53:55 +0700

    edit head

--- my-app/src/Head.js
+++ my-app/src/Head.js
@@ -20,66 +20,36 @@ const head =  () => {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="me-auto">
           <NavDropdown title="Root Equation" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/">
-                Bisection Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/">Bisection Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/FalsePosition">
-                Flase-Position Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/FalsePosition">Flase-Position Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/OnePoint">
-                One-Point Iteration Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/OnePoint">One-Point Iteration Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/TaylorSeries">
-                Taylor Series
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/TaylorSeries">Taylor Series</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/NewtonRapshon">
-                Newton-Rapson Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/NewtonRapshon">Newton-Rapson Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/Secant">
-                Secant Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/Secant">Secant Methods</NavDropdown.Item>
           </NavDropdown>
           <NavDropdown title="Linear Equation" id="collasible-nav-dropdown">
-            <NavDropdown.Item href="/CramerRule">
-                Cramer's Rule
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/CramerRule">Cramer's Rule</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/Gauss">
-                Gauss Elimination Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/Gauss">Gauss Elimination Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/GaussJordan">
-                Gauss Jordan Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/GaussJordan">Gauss Jordan Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/MatrixInversion">
-                Matrix Inversion Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/MatrixInversion">Matrix Inversion Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/LU">
-                LU Decomposition Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/LU">LU Decomposition Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/Cholesky">
-                Cholesky Decomposition Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/Cholesky">Cholesky Decomposition Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/Jacobi">
-                Jacobi Iteration Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/Jacobi">Jacobi Iteration Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/GaussSeidel">
-                Gauss-Seidel Iteration Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/GaussSeidel">Gauss-Seidel Iteration Methods</NavDropdown.Item>
             <NavDropdown.Divider />
-            <NavDropdown.Item href="/Conjugate">
-                Conjugate Gradient Methods
-            </NavDropdown.Item>
+            <NavDropdown.Item href="/Conjugate">Conjugate Gradient Methods</NavDropdown.Item>
           </NavDropdown>
         </Nav>
       </Navbar.Collapse>
