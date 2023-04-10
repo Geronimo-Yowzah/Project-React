16fe984750f5473e835a4bef2367b42998e1712d
Geronimo-Yowzah <panupongpnp02@gmail.com>
2023-04-10 17:15:47 +0700

    test update

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import Bisection from "./RootEquation/Bisection";
+import FalsePosition from "./RootEquation/FalsePosition";
 
 test('renders Bisection Methods', () => {
   render(<Bisection />);
@@ -19,7 +20,7 @@ test('Input Function, XL ,XR is Default' , () =>{
 });
 
 
-test('Input Function, XL ,XR is Not empty', ()=>{
+test('Bisection Input Function, XL ,XR is Not empty', ()=>{
   render(<Bisection />);
 
   const inputFx = screen.getByTestId('equation');
@@ -40,3 +41,27 @@ test('Input Function, XL ,XR is Not empty', ()=>{
   const defaultanswer = screen.getByText(/Answer = 1.89/);
   expect(defaultanswer).toBeInTheDocument();
 });
+
+test('FalsePosition Input Function, XL ,XR is Not empty', ()=>{
+  render(<FalsePosition />);
+
+  const inputFx = screen.getByTestId('equation');
+  fireEvent.change(inputFx,{target: {value: '(x^4)-13'}});
+  expect(inputFx.value).toBe('(x^4)-13');
+
+  const inputXl = screen.getByTestId('XL');
+  fireEvent.change(inputXl,{target: {value: '0'}});
+  expect(inputXl.value).toBe('0');
+
+  const inputXr = screen.getByTestId('XR');
+  fireEvent.change(inputXr,{target: {value: '5'}});
+  expect(inputXr.value).toBe('5');
+
+  const calButton = screen.getByText(/Calculate/);
+  fireEvent.click(calButton);
+
+  const defaultanswer = screen.getByText(/Answer = 1.89/);
+  expect(defaultanswer).toBeInTheDocument();
+});
+
+
